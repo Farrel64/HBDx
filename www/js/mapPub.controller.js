e51091016bc3a6320cf4bdd81a45a54fba4a57af
Thomas Morgana <tmorgana@ippon.fr>
2016-07-21 09:24:53 +0200

    Add link to pub details page on marker click in map page

--- www/js/mapPub.controller.js
+++ www/js/mapPub.controller.js
@@ -1,7 +1,10 @@
 angular.module('mapPub', [])
 
 .controller('mapCtrl', function($scope, $state, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
+  var options = {
+    timeout: 10000,
+    enableHighAccuracy: true
+  };
 
   $scope.pubs = [{
     id: 1,
@@ -14,7 +17,10 @@ angular.module('mapPub', [])
     image: "img/mushroom.jpg",
     mainDrinkType: "Beer",
     secondDrinkType: "Cocktail",
-      position: {lat: 44.84057, lng: -0.58149},
+    position: {
+      lat: 44.84057,
+      lng: -0.58149
+    },
     rating: [4.2]
   }, {
     id: 2,
@@ -27,7 +33,10 @@ angular.module('mapPub', [])
     image: "img/camelot.jpg",
     mainDrink: "Cocktail",
     secondDrinkType: "Beer",
-    position: {lat: 44.83237, lng: -0.57107},
+    position: {
+      lat: 44.83237,
+      lng: -0.57107
+    },
     rating: [3.8]
   }, {
     id: 3,
@@ -40,12 +49,15 @@ angular.module('mapPub', [])
     image: "img/titi.jpg",
     mainDrink: "Beer",
     secondDrinkType: "Wine",
-    position: {lat: 44.83175, lng: -0.56963},
+    position: {
+      lat: 44.83175,
+      lng: -0.56963
+    },
     rating: [4.6, 3.9]
   }, ];
 
 
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
 
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
@@ -58,30 +70,24 @@ angular.module('mapPub', [])
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     //Wait until the map is loaded
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-      for(var i = 0; i<$scope.pubs.length; i++)
-      {
+    google.maps.event.addListenerOnce($scope.map, 'idle', function() {
+      for (var i = 0; i < $scope.pubs.length; i++) {
         console.log(i);
         var marker = new google.maps.Marker({
-            map: $scope.map,
-            animation: google.maps.Animation.DROP,
-            position: $scope.pubs[i].position
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: $scope.pubs[i].position
         });
-        var id = $scope.pubs[i].id;
-        marker.addListener('click', function gotoPub() {
-          $state.go('pub', ({"pubId":id}));
-        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            $state.go('pub', ({
+              "pubId": $scope.pubs[i].id
+            }));
+          }
+        })(marker, i));
       }
-
     });
-
-
-  }, function(error){
+  }, function(error) {
     console.log("Could not get location");
   });
-
-
-
-
-
 });
